62b7d1faaf2cf1c76abbd085ea3415b17283aed9
Xaqui <xoaquin.bg@gmail.com>
2023-04-14 09:55:52 +0200

    Physics tutorial - Step 1.4

---
+++ Movement/Assets/Scripts/Physics/MoveSpherePhysics.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class MoveSpherePhysics : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+    Vector3 velocity, desiredVelocity;
+    Rigidbody body;
+
+    void Awake() {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 acceleration = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+    }
+
+    private void FixedUpdate() {
+        velocity = body.velocity;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        body.velocity = velocity;
+    }
+}
