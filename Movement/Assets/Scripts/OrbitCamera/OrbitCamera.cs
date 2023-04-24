d47a22dd7e4e7dba39e528e57a621d14601ad7d7
Xaqui <xoaquin.bg@gmail.com>
2023-04-24 09:30:10 +0200

    Orbit Camera - Step 2.3

--- Movement/Assets/Scripts/OrbitCamera/OrbitCamera.cs
+++ Movement/Assets/Scripts/OrbitCamera/OrbitCamera.cs
@@ -19,17 +19,50 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField, Range(1f, 20f)]
     float focusCenteringSpeed = 1f;
 
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+
     Vector3 focusPoint;
+    Vector2 orbitAngles = new Vector2(45f, 0f);
 
     void Awake() {
         focusPoint = focus.position;
+        transform.localRotation = Quaternion.Euler(orbitAngles);
     }
 
+    void OnValidate() {
+        if (maxVerticalAngle < minVerticalAngle) {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+
+    void ConstrainAngles() {
+        orbitAngles.x = Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+        if (orbitAngles.y < 0f) {
+            orbitAngles.y += 360f;
+        }
+        else if (orbitAngles.y >= 360f) {
+            orbitAngles.y -= 360f;
+        }
+    }
 
     void LateUpdate() {
         UpdateFocusPoint();
-        Vector3 lookDirection = transform.forward;
-        transform.localPosition = focusPoint - lookDirection * distance;
+        ManualRotation();
+        Quaternion lookRotation;
+        if (ManualRotation()) {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else {
+            lookRotation = transform.localRotation;
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
     }
     void UpdateFocusPoint() {
         Vector3 targetPoint = focus.position;
@@ -47,6 +80,20 @@ public class OrbitCamera : MonoBehaviour
         else {
             focusPoint = targetPoint;
         }
+    }
+
+    /// <returns>true if rotation modified</returns>
+    bool ManualRotation() {
+        Vector2 input = new Vector2(
+            Input.GetAxis("Vertical Camera"),
+            Input.GetAxis("Horizontal Camera")
+        );
+        const float e = 0.001f;
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e) {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            return true;
+        }
+        return false;
     }
 
 }
