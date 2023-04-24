5d9528a30a2af23381b0d052ee8dde468902c126
Xaqui <xoaquin.bg@gmail.com>
2023-04-24 09:09:38 +0200

    Orbit Camra - Step 1.4

--- Movement/Assets/Scripts/OrbitCamera/OrbitCamera.cs
+++ Movement/Assets/Scripts/OrbitCamera/OrbitCamera.cs
@@ -13,6 +13,12 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField, Min(0f)]
     float focusRadius = 1f;
 
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+
+    [SerializeField, Range(1f, 20f)]
+    float focusCenteringSpeed = 1f;
+
     Vector3 focusPoint;
 
     void Awake() {
@@ -29,9 +35,14 @@ public class OrbitCamera : MonoBehaviour
         Vector3 targetPoint = focus.position;
         if (focusRadius > 0f) {
             float distance = Vector3.Distance(targetPoint, focusPoint);
-            if (distance > focusRadius) {
-                focusPoint = Vector3.Lerp(targetPoint, focusPoint, focusRadius / distance);
+            float t = 1f;
+            if (distance > 0.01f && focusCentering > 0f) {
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime * focusCenteringSpeed);
             }
+            if (distance > focusRadius) {
+                t = Mathf.Min(t, focusRadius / distance);
+            }
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
         }
         else {
             focusPoint = targetPoint;
