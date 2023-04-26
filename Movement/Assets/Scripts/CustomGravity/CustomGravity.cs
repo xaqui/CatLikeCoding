4dc8c164a882c9d63a115e618a6d6fb5c8c6f7ea
Xaqui <xoaquin.bg@gmail.com>
2023-04-26 12:52:36 +0200

    Custom Gravity - complete

--- Movement/Assets/Scripts/CustomGravity/CustomGravity.cs
+++ Movement/Assets/Scripts/CustomGravity/CustomGravity.cs
@@ -7,11 +7,13 @@ public static class CustomGravity {
     }
 
     public static Vector3 GetUpAxis(Vector3 position) {
-        return position.normalized;
+        Vector3 up = position.normalized;
+        return Physics.gravity.y < 0f ? up : -up;
     }
     public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis) {
-        upAxis = position.normalized;
-        return upAxis * Physics.gravity.y;
+        Vector3 up = position.normalized;
+		upAxis = Physics.gravity.y < 0f ? up : -up;
+		return up * Physics.gravity.y;
     }
 
 }
