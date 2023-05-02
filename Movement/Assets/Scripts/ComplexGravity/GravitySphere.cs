8562c780ba70b9e950eb20d3210019cd9f076b0b
Xaqui <xoaquin.bg@gmail.com>
2023-05-02 10:06:35 +0200

    Complex Gravity - Step 3.3

--- Movement/Assets/Scripts/ComplexGravity/GravitySphere.cs
+++ Movement/Assets/Scripts/ComplexGravity/GravitySphere.cs
@@ -8,11 +8,21 @@ public class GravitySphere : GravitySource {
     [SerializeField, Min(0f)]
     float outerRadius = 10f, outerFalloffRadius = 15f;
 
-    float outerFalloffFactor;
+    [SerializeField, Min(0f)]
+    float innerFalloffRadius = 1f, innerRadius = 5f;
+
+    float innerFalloffFactor, outerFalloffFactor;
 
     void OnDrawGizmos() {
         Vector3 p = transform.position;
+        if (innerFalloffRadius > 0f && innerFalloffRadius < innerRadius) {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(p, innerFalloffRadius);
+        }
         Gizmos.color = Color.yellow;
+        if (innerRadius > 0f && innerRadius < outerRadius) {
+            Gizmos.DrawWireSphere(p, innerRadius);
+        }
         Gizmos.DrawWireSphere(p, outerRadius);
         if (outerFalloffRadius > outerRadius) {
             Gizmos.color = Color.cyan;
@@ -22,12 +32,14 @@ public class GravitySphere : GravitySource {
     public override Vector3 GetGravity(Vector3 position) {
         Vector3 vector = transform.position - position;
         float distance = vector.magnitude;
-        if (distance > outerFalloffRadius) {
+        if (distance > outerFalloffRadius || distance < innerFalloffRadius) {
             return Vector3.zero;
         }
         float g = gravity / distance;
         if (distance > outerRadius) {
             g *= 1f - (distance - outerRadius) * outerFalloffFactor;
+        } else if (distance < innerRadius) {
+            g *= 1f - (innerRadius - distance) * innerFalloffFactor;
         }
         return g * vector; 
     }
@@ -36,7 +48,12 @@ public class GravitySphere : GravitySource {
         OnValidate();
     }
     void OnValidate() {
+        innerFalloffRadius = Mathf.Max(innerFalloffRadius, 0f);
+        innerRadius = Mathf.Max(innerRadius, innerFalloffRadius);
+        outerRadius = Mathf.Max(outerRadius, innerRadius);
         outerFalloffRadius = Mathf.Max(outerFalloffRadius, outerRadius);
+
+        innerFalloffFactor = 1f / (innerRadius - innerFalloffRadius);
         outerFalloffFactor = 1f / (outerFalloffRadius - outerRadius);
     }
 
